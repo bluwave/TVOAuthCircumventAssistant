1d0914ee95eee6911fe0ca5e30e4734a3e62a1c1
Garrett Richards <garrettrichards@gmail.com>
2016-01-02 21:40:07 -0800

    added example

--- TVOAuthCircumventAssistant/TVOAuthCircumventAssistant/ViewController.swift
+++ TVOAuthCircumventAssistant/TVOAuthCircumventAssistant/ViewController.swift
@@ -10,16 +10,56 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var URLLabel: UILabel!
+    
+    private var assistant: TVOAuthCircumventAssistant?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        configureAssistant()
+        
+    }
+    
+    private func configureAssistant() {
+        
+        var configuration = TVOAuthCircumventAssistantConfiguration()
+
+        //  This URL below is contrived to illustrate how to use this example, usually the response would give you a tvToken
+        //  you could use with your app
+        let url:String = "http://bit.ly/123456".stringByAddingPercentEncodingWithAllowedCharacters(.URLHostAllowedCharacterSet())!
+        configuration.tvTokenFetchURL = "https://httpbin.org/response-headers?tvToken=123456&url=\(url)"
+        configuration.retrieveAuthenticationInfoURL = "https://httpbin.org/response-headers?accessToken=abcdefghijk123456&userid=1"
+
+        assistant = TVOAuthCircumventAssistant(configuration: configuration)
+        
+        assistant?.fetchTVToken({ [weak self] (response, error) -> Void in
+            if let response = response {
+                self?.URLLabel.text = response["url"] as? String
+                if let tvToken = response["tvToken"] {
+                    //  This URL is contrived again to illustrate use of the response from this request to check
+                    //  that the user has successfully authenticated on phone
+                    self?.assistant?.configuration.retrieveAuthenticationInfoURL = (self?.assistant?.configuration.retrieveAuthenticationInfoURL)! + "&tvToken=\(tvToken)"
+                }
+            }
+            else if let error = error as? NSError {
+                self?.URLLabel.text = error.localizedDescription
+                print("error: \(error.localizedDescription)")
+            }
+        })
+    }
+    
+    private func retrieveOAuthAuthenticationInfo() {
+        assistant?.retrieveAuthenticationInfoURL({ (response, error) -> Void in
+            if let response = response {
+                print("OAuth authentication info: \(response)")
+            } else if let error = error as? NSError {
+                print("error: \(error.localizedDescription)")
+            }
+        })
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func actionDoneAuthenticatingWithOAuthProvider(sender: UIButton) {
+        retrieveOAuthAuthenticationInfo()
     }
-
-
 }
 
